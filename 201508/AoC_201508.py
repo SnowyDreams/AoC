e9ae1cdbdc2a2e7385bf548ad9dfa0a09217e6dd
Dor Prizant <dorprizan@gmail.com>
2023-01-28 19:28:30 +0200

    - 2015: Day 8, Part 2

--- 201508/AoC_201508.py
+++ 201508/AoC_201508.py
@@ -1,13 +1,30 @@
 input = open("input.txt","r").read()
+# input = open("test_input.txt","r").read()
+
+escapes = ['\x22','\x5C']
 literalLength = 0
 memoryLength = 0
+rencodedLength = 0
+
+def rencode(line):
+    rencoded = []
+    for char in line:
+        if char in escapes:
+            rencoded.append('\x5C')
+            rencoded.append(char)
+        else:
+            rencoded.append(char)
+    return("".join(rencoded).__len__())
 
 for line in input.splitlines():
     inLiteral = line.__len__()
     inMemory = eval(line).__len__()
+    rencoded = int(rencode(line)) +2
     literalLength += inLiteral
     memoryLength += inMemory
-    # print(f"{line} is {inLiteral} in literal and {inMemory} in memory")
+    rencodedLength += rencoded
+    # print(f"{line} is {inLiteral} in literal and {inMemory} in memory and {rencoded} when re-encoded")
 
-print(literalLength-memoryLength)
+print(f"Solution for first part is: {literalLength-memoryLength}")
+print(f"Solution for second part is: {rencodedLength-literalLength}")
 
