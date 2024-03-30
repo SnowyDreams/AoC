0e5d10cb6b8e848fdb7dbe0df29252aecdbdff09
Dor Prizant <dorprizan@gmail.com>
2024-03-30 19:08:31 +0300

    - 2015: Day 10, done

--- 201510/AoC_201510.py
+++ 201510/AoC_201510.py
@@ -1,32 +1,31 @@
-exampleInput = '1211'
-puzzleInput = '1113222113'
-repeat = 1
-result = ''
-
-result = exampleInput
-for x in range(repeat):
-    manipulated = ''
-    lastDig = ''
+def count_chars(char,index,sequence):
     count = 0
-    for i in range(0,result.__len__()+1):
-        if i == result.__len__():
-            count = 1 if count == 0 else count
-            lastDig = result[i-1]
-            manipulated += f'{str(count)}{lastDig}'
-            continue
-
-        if lastDig == '':
-            lastDig = result[i-1] if i-1 > 0 else result[i]
-
-        if result[i] == lastDig:
+    for x in range(len(sequence) - index):
+        if char == sequence[index+x]:
             count += 1
-        
-        if result[i] != lastDig:
-            manipulated += f'{str(count)}{lastDig}'
-            count = 0
             continue
-        # manipulated += f'{str(count)}{lastDig}'
-    print(manipulated)
+        break
+    return count if count != 0 else 1
 
+def look_and_say(sequence):
+    result = ''
+    x = 0
+    while x < len(sequence):
+        index = count_chars(sequence[x],x,sequence)
+        result += f'{index}{sequence[x]}'
+        if x + index >= len(sequence):
+            break
+        x += index
+    return result
 
+puzzle = '1113222113'
+repeat = 50
+output = ''
+
+for loop in range(repeat):
+    output = look_and_say(puzzle)
+    puzzle = output
+
+print(len(output))
     
+
